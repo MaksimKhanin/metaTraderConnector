443df36b5fc2b360322284eafb284d97b716b60f
MaksimKhanin <45826272+MaksimKhanin@users.noreply.github.com>
2020-08-06 23:12:56 +0300

    Update mt5_connector.py

--- mt5_connector.py
+++ mt5_connector.py
@@ -2,6 +2,20 @@ import MetaTrader5 as mt5
 
 
 class mt5_connector:
+    
+    """
+    USAGE:
+    
+    my_terminal = mt5_connector(server='MyBrokerOrDealerServer',
+                                 login=40927195,
+                                 password='pdfpfdpdf',
+                                 path="C:/metatrader/terminal64.exe",
+                                 timedelta=3)
+
+    my_terminal.open_position("BUY", "EURUSD", filltype=1, lot=0.01, magic=4577)
+    last_positions = my_terminal.get_positions("EURUSD")[0]
+    my_terminal.close_position(last_positions, filltype=1)
+    """
 
     def __init__(self, server, login, password, path, timedelta=3):
 
